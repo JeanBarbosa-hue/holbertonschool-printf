119e48a6acc22ec03fa53551726340a127212964
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-28 00:54:57 +0000

    try 1 of exercise 0

---
+++ print_functions.c
@@ -0,0 +1,26 @@
+#include "printf.h"
+#include<stdio.h>
+
+int print_letter(char c)
+{
+	putchar(c);
+		return(1);
+}
+
+int print_string(char *c)
+{
+	int k = 0;
+
+	while (c[k] != '\0')
+	{
+		putchar(c[k]);
+		k++;
+	}
+	return(k);
+}
+
+int print_percentage()
+{
+	putchar('%');
+	return(1);
+}
---
+++ printf.h
@@ -0,0 +1,9 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+int _printf(const char *format, ...);
+int print_string(char *c);
+int print_letter(char c);
+int print_percentage();
+
+#endif
