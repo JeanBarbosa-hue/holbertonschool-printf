391a83c6c2efd40355e39e68b4fbca99812ebf78
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-29 19:12:33 +0000

    char str int % default implemented correctly

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+int print_letter(int);
+int print_string(char *);
+int print_percentage();
+int print_integer(int);
+
+#endif 
